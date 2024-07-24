51d0c0ccb497834248b980eb89c40f0797397b43
Meibaybay <168160954+Meibaybay@users.noreply.github.com>
2024-07-24 10:50:48 +0700

    Delete example11.java

--- example11.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-public class example11 {
-    public static void main (String [] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first floating-point number: ");
-
-    }
-}
